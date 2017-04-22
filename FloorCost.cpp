2992fc035126d7e5b026873e4da7758ab8c2685f
Aaron <abrunette@users.noreply.github.com>
2017-04-22 09:39:59 -0700

    Update FloorCost.cpp

--- FloorCost.cpp
+++ FloorCost.cpp
@@ -18,6 +18,9 @@ double calculateTotal(double, double, double);
 //Purpose: To recieve user input regarding the width, height,
 // and cost of a floor plan. It then calls a function to 
 // calculate the total cost to tile the floor plan.
+//User information: Run the program.
+//Input: Takes three positive numbers, width, height, and cost.
+//Output: Outputs the combined total of the three previous values.
 int main()
 {
 	double width, height, cost;	
@@ -42,10 +45,8 @@ int main()
 
 //Purpose: To calculate the total cost needed to tile
 // a floor of W width, and H height, at C cost.
-
 //Parameters: W is width, H is height, C is cost.
-
-//Algorith: Multiplication of 3 variables. Returns immediately.
+//Algorithm: Multiplication of 3 variables. Returns immediately.
 double calculateTotal(double W, double H, double C)
 {
 	return ((W * H) * C);
