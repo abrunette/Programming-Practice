025378dc16d34174fb68fafedcb8c4db75c6c9ca
Aaron <abrunette@users.noreply.github.com>
2017-04-22 18:16:40 -0700

    Create Bin2Dec.cpp

---
+++ Bin2Dec.cpp
@@ -0,0 +1,20 @@
+/*****************filename**************/
+//Author: Aaron Brunette
+//Github: abrunette
+//Last updated: 20YY/MM/DD
+//Compiled with
+//Written on 
+//Purpose:
+/***************************************/
+
+#include <iostream>   //input/output library
+#include <iomanip>    //input/output manipulation library
+#include <cmath>      //standard C math library
+
+//Purpose:
+//User information:
+int main()
+{
+
+
+}
